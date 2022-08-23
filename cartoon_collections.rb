42f34dc9267896064f1fd69db90dafa862a2b570
Andrew Stahl <andrewstahl96@gmail.com>
2022-08-23 19:02:53 -0400

    commit tree 09874ccf2db9d941be5767be1fe888ecefd7fa83

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf_list)
+  dwarf_list.each_with_index do |dwarf, index|
+    puts "#{index +  1}. #{dwarf}"
+  end
+end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map{ |call| "#{call[0].upcase + call[1..-1]}!" }
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  (planeteer_calls).any?{ |call| call.length > 4  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  combined_array = snacks & cheese_types
+  (combined_array).empty? ? nil : combined_array[0]
 end
